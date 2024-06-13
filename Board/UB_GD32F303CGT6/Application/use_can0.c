a82a8a978274b7c9915ab96e1f02822056b21b6c
Chen Peng <pizer.chen@gmail.com>
2024-06-13 09:55:34 +0800

    采集CAN数据，并进行数据压缩传输

--- Board/UB_GD32F303CGT6/Application/use_can0.c
+++ Board/UB_GD32F303CGT6/Application/use_can0.c
@@ -5,6 +5,7 @@
 #include <bsp_tim6.h>
 #include "meter_protocol.h"
 #include "global.h"
+#include "CAN.h"
 #include <mcu_protocol.h>
 #include <sdk_crc16.h>
 
@@ -38,10 +39,14 @@ static int USE_CAN0_MeterProtocl_WriteIdx = 0;
 
 static USE_CAN0_OnTimeout_Handler USE_CAN0__OnTimeoutHandler=0;
 static void* USE_CAN0__OnTimeoutHandler_Userdata=0;
+static mcu_protocol_t mcu_tx_protocol;
+static CAN_Parse_Session_T USE_CAN0__CAN_ParseSession;
 ////////////////////////////////////////////////////////////////////////////////
 ////
 
 static void USE_CAN0__HandleRxMessage(bool isTimeout);
+
+static meter_protocol_t *USE_CAN0__SendCAN(meter_protocol_t *protocol, const global_t *global, uint32_t tick);
 
 static void USE_CAN0_RxHandler(can_receive_message_struct* rxMsg, void* userdata)
 {
@@ -50,6 +55,7 @@ static void USE_CAN0_RxHandler(can_receive_message_struct* rxMsg, void* userdata
     if(!msg){return;}
     memcpy(&msg->can_msg, rxMsg, sizeof(*rxMsg));
     msg->tick_stamp = tick;
+    os_semaphore_release(&USE_CAN0_RxSem);
 }
 
 static void USE_CAN0_RxThread_Entry(void* p){
@@ -65,6 +71,7 @@ static void USE_CAN0_RxThread_Entry(void* p){
         }
     }
 }
+static char USE_CAN0__Hex[17]={0};
 
 static void USE_CAN0__HandleRxMessage(bool isTimeout) {
     meter_protocol_t* protocol = &USE_CAN0__MeterProtocolBuffers[USE_CAN0_MeterProtocl_WriteIdx];
@@ -76,78 +83,84 @@ static void USE_CAN0__HandleRxMessage(bool isTimeout) {
         if(!msg){
             continue;
         }
+
+#if 0
+        memset(USE_CAN0__Hex, 0, sizeof(USE_CAN0__Hex));
+        SDK_HEX_ENCODE_BE(USE_CAN0__Hex, sizeof(USE_CAN0__Hex), msg->can_msg.rx_data, msg->can_msg.rx_dlen);
+        mcu_protocol_du_print(&mcu_protocol_g_tx_protocol, USE_CAN0__Hex, strlen(USE_CAN0__Hex));
+#endif
+
         uint32_t tick = BSP_TIM6__TickCount;
+        uint32_t FrameID = (msg->can_msg.rx_ff==CAN_FF_STANDARD)?msg->can_msg.rx_sfid:msg->can_msg.rx_efid;
+        uint8_t TPCM_Type = CAN_GetTPCMType(FrameID, msg->can_msg.rx_data, msg->can_msg.rx_dlen);
 
-        METER_PROTOCOL_CAN_LINE_FILL(protocol, protocol->lines, global->datetime.year
-        , global->datetime.month
-        , global->datetime.date
-        , global->datetime.hour
-        , global->datetime.min
-        , global->datetime.sec
-        , tick
-        , (msg->can_msg.rx_ff==CAN_FF_STANDARD)?msg->can_msg.rx_sfid:msg->can_msg.rx_efid
-        , msg->can_msg.rx_dlen
-        , msg->can_msg.rx_data
-        );
-        protocol->lines+=1;
-
-        if(protocol->lines==USE_CAN0_MAX_LINES){
-            METER_PROTOCOL_CAN_INIT(protocol, global->CPUID);
-            METER_PROTOCOL_DUSIZE_SET(protocol, protocol->lines*24 + 10);
-            METER_PROTOCOL_CAN_LINES_SET(protocol, protocol->lines);
-            METER_PROTOCOL_DATETIME_SET(protocol, 31, global->datetime.year
+        if(TPCM_Type!=CAN_TPCM_UNKNOWN){
+            /*分包类型直接发送*/
+            METER_PROTOCOL_CAN_LINE_FILL(protocol, protocol->lines, global->datetime.year
             , global->datetime.month
             , global->datetime.date
             , global->datetime.hour
             , global->datetime.min
             , global->datetime.sec
-            , tick);
-            int buffer_size = protocol->lines * 24 + 42;
-            uint16_t crc = sdk_crc16(protocol->buffer, buffer_size);
-            SDK_HEX_SET_UINT16_LE(protocol->buffer, buffer_size, crc);
+            , tick
+            , FrameID
+            , msg->can_msg.rx_dlen
+            , msg->can_msg.rx_data
+            );
+            protocol->lines+=1;
+        }else{
+            /*不是分包*/
+            int updated = CAN_Parse(&USE_CAN0__CAN_ParseSession, FrameID, msg->can_msg.rx_data, msg->can_msg.rx_dlen);
+            if(updated==0) continue; /*没有更新*/
+            METER_PROTOCOL_CAN_LINE_FILL(protocol, protocol->lines, global->datetime.year
+            , global->datetime.month
+            , global->datetime.date
+            , global->datetime.hour
+            , global->datetime.min
+            , global->datetime.sec
+            , tick
+            , FrameID
+            , msg->can_msg.rx_dlen
+            , msg->can_msg.rx_data
+            );
+            protocol->lines+=1;
+        }
 
-            mcu_protocol_can_init(&mcu_protocol_g_tx_protocol, protocol->buffer, buffer_size + 2);
-            mcu_protocol_send(&mcu_protocol_g_tx_protocol);
-
-            int next_idx = USE_CAN0_MeterProtocl_WriteIdx;
-            if(next_idx==USE_CAN0_MAX_METER_PROTOCOL_BUFFER_COUNT){
-                next_idx = 0;
-            }
-            USE_CAN0_MeterProtocl_WriteIdx = next_idx;
-            protocol = &USE_CAN0__MeterProtocolBuffers[USE_CAN0_MeterProtocl_WriteIdx];
-            protocol->lines = 0;
-
+        if(protocol->lines==USE_CAN0_MAX_LINES+1){
+            protocol = USE_CAN0__SendCAN(protocol, global, tick);
         }
     }
 
-    if(isTimeout && protocol->lines!=0 && protocol->lines<USE_CAN0_MAX_LINES){
-        uint32_t tick = BSP_TIM6__TickCount;
-
-        METER_PROTOCOL_CAN_INIT(protocol, global->CPUID);
-        METER_PROTOCOL_DUSIZE_SET(protocol, protocol->lines*24 + 10);
-        METER_PROTOCOL_CAN_LINES_SET(protocol, protocol->lines);
-        METER_PROTOCOL_DATETIME_SET(protocol, 31, global->datetime.year
-        , global->datetime.month
-        , global->datetime.date
-        , global->datetime.hour
-        , global->datetime.min
-        , global->datetime.sec
-        , tick);
-        int buffer_size = protocol->lines * 24 + 42;
-        uint16_t crc = sdk_crc16(protocol->buffer, buffer_size);
-        SDK_HEX_SET_UINT16_LE(protocol->buffer, buffer_size, crc);
-
-        mcu_protocol_can_init(&mcu_protocol_g_tx_protocol, protocol->buffer, buffer_size + 2);
-        mcu_protocol_send(&mcu_protocol_g_tx_protocol);
-
-        int next_idx = USE_CAN0_MeterProtocl_WriteIdx;
-        if(next_idx==USE_CAN0_MAX_METER_PROTOCOL_BUFFER_COUNT){
-            next_idx = 0;
+    if(isTimeout){
+        if(protocol->lines>0){
+            uint32_t tick = BSP_TIM6__TickCount;
+            protocol = USE_CAN0__SendCAN(protocol, global, tick);
         }
-        USE_CAN0_MeterProtocl_WriteIdx = next_idx;
-        protocol = &USE_CAN0__MeterProtocolBuffers[USE_CAN0_MeterProtocl_WriteIdx];
-        protocol->lines = 0;
+        CAN_Parse_Session_Init(&USE_CAN0__CAN_ParseSession);
     }
+}
+
+static meter_protocol_t *USE_CAN0__SendCAN(meter_protocol_t *protocol, const global_t *global, uint32_t tick) {
+    METER_PROTOCOL_CAN_INIT(protocol, global->CPUID);
+    METER_PROTOCOL_DUSIZE_SET(protocol, protocol->lines * 24 /*每行字节数*/ + 11/*时间，数据类型，数据条数*/);
+    METER_PROTOCOL_CAN_LINES_SET(protocol, protocol->lines);
+    METER_PROTOCOL_DATETIME_SET(protocol, 31, global->datetime.year, global->datetime.month, global->datetime.date,
+                                global->datetime.hour, global->datetime.min, global->datetime.sec, tick);
+    int buffer_size = protocol->lines * 24 /*每行字节数*/ + 42 /* 头部字节数 */;
+    uint16_t crc = sdk_crc16(protocol->buffer, buffer_size);
+    SDK_HEX_SET_UINT16_LE(protocol->buffer, buffer_size, crc);
+
+    mcu_protocol_can_init(&mcu_tx_protocol, protocol->buffer, buffer_size + 2);
+    mcu_protocol_send(&mcu_tx_protocol);
+
+    int next_idx = USE_CAN0_MeterProtocl_WriteIdx;
+    if(next_idx==USE_CAN0_MAX_METER_PROTOCOL_BUFFER_COUNT){
+        next_idx = 0;
+    }
+    USE_CAN0_MeterProtocl_WriteIdx = next_idx;
+    protocol = &USE_CAN0__MeterProtocolBuffers[USE_CAN0_MeterProtocl_WriteIdx];
+    protocol->lines = 0;
+    return protocol;
 }
 
 
@@ -157,6 +170,9 @@ static void USE_CAN0__HandleRxMessage(bool isTimeout) {
 
 void USE_CAN0_Init(void)
 {
+
+    CAN_Parse_Session_Init(&USE_CAN0__CAN_ParseSession);
+
     memset(USE_CAN0__MeterProtocolBuffers, 0, sizeof(*USE_CAN0__MeterProtocolBuffers) * OS_ARRAY_SIZE(USE_CAN0__MeterProtocolBuffers));
     USE_CAN0__OnTimeoutHandler = 0;
     USE_CAN0__OnTimeoutHandler_Userdata = 0;
