d94748a3fc46a53d194e01af4fc6f3e48b05b5e2
Chen Peng <pizer.chen@gmail.com>
2024-06-13 16:40:48 +0800

    按键优化

--- Board/UB_GD32F470VGT6/Application/use_key.c
+++ Board/UB_GD32F470VGT6/Application/use_key.c
@@ -2,14 +2,22 @@
 #include <os_kernel.h>
 #include <bsp_lock.h>
 
+#define USE_KEY_UNLOCK 0
+#define USE_KEY_LOCKED 1
+
+static int USE_KEY__State = USE_KEY_UNLOCK;
+
 void USE_KEY_OnKeyDownHandler(int state)
 {
-    printf("[USE_KEY] State = %d\n", state);
     for(int i=0; i<0x3ffff;i++);
     int state2 = BSP_Key_State();
     if(state2==1){
+        if(USE_KEY__State == USE_KEY_LOCKED) return;
+        USE_KEY__State = USE_KEY_LOCKED;
         BSP_Lock_Enable();
     }else{
+        if(USE_KEY__State == USE_KEY_UNLOCK) return;
+        USE_KEY__State = USE_KEY_UNLOCK;
         BSP_Lock_Disable();
     }
 }
