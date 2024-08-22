0c9268cd34dc1400e6615e4c635b2dc8a7fd0240
Chen Peng <pizer.chen@gmail.com>
2024-08-22 18:05:17 +0800

    critical refine

--- Kernel/os_critical.h
+++ Kernel/os_critical.h
@@ -11,14 +11,26 @@
 #include <os_scheduler.h>
 #endif /*INCLUDED_OS_SCHEDULER_H*/
 
+#ifndef INCLUDED_CPU_H
+#include <cpu.h>
+#endif /*INCLUDED_CPU_H*/
+
+
+
 ////////////////////////////////////////////////////////////////////////////////
 ////
 C_STATIC_FORCEINLINE void os_critical_enter(void){
+    cpu_interrupt_context_t ctx;
+    cpu_interrupt_disable(&ctx);
     os_scheduler__lock_nest++;
+    cpu_interrupt_enable(&ctx);
 }
 
 C_STATIC_FORCEINLINE void os_critical_leave(void){
+    cpu_interrupt_context_t ctx;
+    cpu_interrupt_disable(&ctx);
     os_scheduler__lock_nest--;
+    cpu_interrupt_enable(&ctx);
 }
 
 
